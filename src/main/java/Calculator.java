d560769d8c70a1491a618b9ea27053cb7a7bbe06
guiji <1873447583@qq.com>
2023-07-01 22:45:51 +0800

    add calculator code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,13 @@
 public class Calculator {
     public int compute(int num1, int num2, char symbo) {
 
-        return -1;
+        switch (symbo) {
+            case '+' :
+                return num1 + num2;
+            case '-' :
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
